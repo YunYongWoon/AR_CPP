53e3809a26c85020ba5feb906e82f1cd7f722c87
Yun_Yongwoon <yuyun0124@gmail.com>
2019-02-18 21:39:28 +0900

    변수

--- BCSD/Main.cpp
+++ BCSD/Main.cpp
@@ -2,5 +2,17 @@
 using namespace std;
 
 int main() {
+	int NUMBER = 20;
+	cout << NUMBER << endl;
+	
+	bool TFtest = true;
+	cout << TFtest << endl;
+
+	char cTest = 't';
+	cout << cTest << endl;
+
+	float fNumber = 3.14;
+	cout << fNumber << endl;
+
 	return 0;
 }
